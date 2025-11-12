a6bee25d94c9b6e7965955b08768eec1c4f63b46
antonk <anton.khazan@gmail.com>
2025-11-12 11:32:52 +0200

    hfsc-no-json: double-quote args

--- hfsc-no-json.sh
+++ hfsc-no-json.sh
@@ -15,46 +15,46 @@ create_qdisc() {
 
 apply_rules_no_json() {
 	# Indentation expresses class/qdisc/filters hierarchy position
-	create_qdisc root 1: &&
+	create_qdisc root "1:" &&
 
-		{ [ "$DIR" = UP ] || create_class hfsc_lan 1:2 1: ; } &&
+		{ [ "$DIR" = UP ] || create_class "hfsc_lan" "1:2" "1:" ; } &&
 
-		create_class "hfsc_main_link" 1:1 1: &&
+		create_class "hfsc_main_link" "1:1" "1:" &&
 
-			create_class "hfsc_tin fast" 1:12 1:1 &&
-				create_qdisc "hfsc_non_game" "" 1:12 &&
+			create_class "hfsc_tin fast" "1:12" "1:1" &&
+				create_qdisc "hfsc_non_game" "" "1:12" &&
 				for family in ipv4 ipv6; do
 					create_filters "CS4 AF41 AF42" "1:12" "$family" || return 1
 				done &&
 
-			create_class "hfsc_tin normal" 1:13 1:1 &&
-				create_qdisc "hfsc_non_game" "" 1:13 &&
+			create_class "hfsc_tin normal" "1:13" "1:1" &&
+				create_qdisc "hfsc_non_game" "" "1:13" &&
 				for family in ipv4 ipv6; do
 					create_filters "CS0" "1:13" "$family" || return 1
 				done &&
 
-			create_class "hfsc_tin lowprio" 1:14 1:1 &&
-				create_qdisc "hfsc_non_game" "" 1:14 &&
+			create_class "hfsc_tin lowprio" "1:14" "1:1" &&
+				create_qdisc "hfsc_non_game" "" "1:14" &&
 				for family in ipv4 ipv6; do
 					create_filters "CS2 AF11" "1:14" "$family" || return 1
 				done &&
 
-			create_class "hfsc_tin bulk" 1:15 1:1 &&
-				create_qdisc "hfsc_non_game" "" 1:15 &&
+			create_class "hfsc_tin bulk" "1:15" "1:1" &&
+				create_qdisc "hfsc_non_game" "" "1:15" &&
 				for family in ipv4 ipv6; do
 					create_filters "CS1" "1:15" "$family" || return 1
 				done &&
 
-			create_class "hfsc_tin realtime" 1:11 1:15 &&
-				create_qdisc "hfsc_game" 10: 1:11 &&
+			create_class "hfsc_tin realtime" "1:11" "1:15" &&
+				create_qdisc "hfsc_game" "10:" "1:11" &&
 					case "$gameqdisc" in
 						drr|qfq)
-							create_class "drr_qfq" 8000 10:1 10: &&
-								create_qdisc "red" 11: 10:1 &&
-							create_class "drr_qfq" 4000 10:2 10: &&
-								create_qdisc "red" 12: 10:2 &&
-							create_class "drr_qfq" 1000 10:3 10: &&
-								create_qdisc "red" 13: 10:3 ;;
+							create_class "drr_qfq" 8000 "10:1" "10:" &&
+								create_qdisc "red" "11:" "10:1" &&
+							create_class "drr_qfq" 4000 "10:2" "10:" &&
+								create_qdisc "red" "12:" "10:2" &&
+							create_class "drr_qfq" 1000 "10:3" "10:" &&
+								create_qdisc "red" "13:" "10:3" ;;
 						*) :
 					esac
 }
